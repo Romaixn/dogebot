5ae9c12c403c6c0f6b503136553a8debda79ea2f
Romain <romain.herault99@gmail.com>
2019-01-23 21:28:10 +0100

    :alien: update cat command

--- commands/animals/cat.js
+++ commands/animals/cat.js
@@ -1,28 +1,32 @@
 const commando = require('discord.js-commando');
 const Discord = require('discord.js');
-const randomPuppy = require('random-puppy');
+const request = require('request');
 
 class CatCommand extends commando.Command {
-  constructor(client) {
-    super(client, {
-      name: 'cat',
-      group: 'animals',
-      memberName: 'cat',
-      description: 'Doge t\'enverra une image mignonne d\'un chat',
-      examples: ['!cat']
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'cat',
+            group: 'animals',
+            memberName: 'cat',
+            description: 'Doge t\'enverra une image d\'un chat',
+            examples: ['!cat']
+        });
+    }
 
-  run(message, args) {
-    randomPuppy('cat')
-      .then(url => {
-        const embed = new Discord.RichEmbed()
-          .setTitle("Chat")
-          .setColor(0x00AE86)
-          .setImage(url);
-          message.channel.send({ embed });
-      });
-  }
+    run(message, args) {
+        request({
+            url: "https://aws.random.cat/meow?ref=public-apis",
+            json : true
+        }, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                const embed = new Discord.RichEmbed()
+                      .setTitle("Chat")
+                      .setColor(0x00AE86)
+                      .setImage(body['file']);
+                message.channel.send({ embed });
+            }
+        });
+    }
 }
 
 module.exports = CatCommand;
