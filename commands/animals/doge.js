7c0542d107da2f51a930908fd9f71954a674333f
Romain <romain.herault99@gmail.com>
2019-01-23 21:28:34 +0100

    :alien: update doge command

--- commands/animals/doge.js
+++ commands/animals/doge.js
@@ -1,6 +1,6 @@
 const commando = require('discord.js-commando');
 const Discord = require('discord.js');
-const randomPuppy = require('random-puppy');
+const request = require('request');
 
 class DogeCommand extends commando.Command {
     constructor(client) {
@@ -8,20 +8,24 @@ class DogeCommand extends commando.Command {
             name: 'doge',
             group: 'animals',
             memberName: 'doge',
-            description: 'Doge t\'enverra une image de lui même. Incroyable non ?',
+            description: 'Doge t\'enverra une image d\'un putain de chien',
             examples: ['!doge']
         });
     }
 
     run(message, args) {
-        randomPuppy('ShibaInu')
-            .then(url => {
+        request({
+            url: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false",
+            json : true
+        }, function(error, response, body){
+            if(!error && response.statusCode == 200){
                 const embed = new Discord.RichEmbed()
-                      .setTitle("Putain de doge")
+                      .setTitle("Doge")
                       .setColor(0x00AE86)
-                      .setImage(url);
+                      .setImage(body['0']);
                 message.channel.send({ embed });
-            });
+            }
+        });
     }
 }
 
